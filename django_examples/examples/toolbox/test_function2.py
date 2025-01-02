144b70aec757686bc446cf51259430b64442ff75
ian_jones <ian@cobi.co.uk>
2025-01-02 21:16:32 +0000

    Test button colour

--- django_examples/examples/toolbox/test_function2.py
+++ django_examples/examples/toolbox/test_function2.py
@@ -4,6 +4,7 @@ from toolbox_view import ToolBoxBase
 class DemoFile2(ToolBoxBase):
     button_text = 'Another file'
     button_group = 'Named Group'
+    button_colour = 'danger'
 
     def button_function(self):
         return self.command_response('message', text='Another File')
